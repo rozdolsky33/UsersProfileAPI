57034ed0a72ce5c7783bb491a6e275db48eca864
volodymyrrozdolsky <rozdolskyvolodymyr@gmail.com>
2020-03-19 09:53:32 -0400

    initial setup

---
+++ src/main/java/com/arwest/developer/profile_app_api/ProfileAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.arwest.developer.profile_app_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileAppApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfileAppApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arwest/developer/profile_app_api/ProfileAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arwest.developer.profile_app_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileAppApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
